ad00d259e442a362344c8484be449ed4334f1491
runlinwang <rwang3@college.harvard.edu>
2022-10-15 18:42:21 -0400

    completed files

---
+++ server/main.js
@@ -0,0 +1,48 @@
+import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
+import { TasksCollection } from '/imports/db/TasksCollection';
+import '/imports/api/tasksMethods';
+import '/imports/api/tasksPublications';
+
+const insertTask = (taskText, user) =>
+  TasksCollection.insert({
+    text: taskText,
+    userId: user._id,
+    createdAt: new Date(),
+  });
+
+const SEED_USERNAME = 'guest';
+const SEED_PASSWORD = 'unproductive';
+
+const MY_USERNAME = 'runlin';
+const MY_PASSWORD = 'productive';
+
+Meteor.startup(() => {
+  if (!Accounts.findUserByUsername(SEED_USERNAME)) {
+    Accounts.createUser({
+      username: SEED_USERNAME,
+      password: SEED_PASSWORD,
+    });
+  }
+
+  if (!Accounts.findUserByUsername(MY_USERNAME)) {
+    Accounts.createUser({
+      username: MY_USERNAME,
+      password: MY_PASSWORD,
+    });
+  }
+
+  const user = Accounts.findUserByUsername(SEED_USERNAME);
+
+  if (TasksCollection.find().count() === 0) {
+    [
+      'First Task',
+      'Second Task',
+      'Third Task',
+      'Fourth Task',
+      'Fifth Task',
+      'Sixth Task',
+      'Seventh Task',
+    ].forEach(taskText => insertTask(taskText, user));
+  }
+});
